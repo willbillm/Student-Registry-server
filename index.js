f7e929f3841f3464205461ecc757f37a136bdc5f
willbillm <mcginnis.william1@gmail.com>
2022-10-20 14:35:38 -0700

    env added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import studentRoutes from './routes/student.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -13,7 +16,7 @@ app.use(cors());
 app.use('/students', studentRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://ragZnib:ragZ259177@cluster0.yb5t7.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yb5t7.mongodb.net/?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
